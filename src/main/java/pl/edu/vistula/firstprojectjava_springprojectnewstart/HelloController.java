be40b1aaf5d4739076734e78ff8935765e6c6926
Mr-Black-droid <tadiscar2.0@gmail.com>
2025-05-22 21:39:58 +0200

    Update HelloController.java

--- src/main/java/pl/edu/vistula/firstprojectjava_springprojectnewstart/HelloController.java
+++ src/main/java/pl/edu/vistula/firstprojectjava_springprojectnewstart/HelloController.java
@@ -16,8 +16,8 @@ public class HelloController {
     }
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greeting(@RequestParam(name = "Tadiwanashe", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("Tadiwanashe", name);
         return "greeting";
     }
 }
